e1173c127502534e44b935289ba94cabcc8ed835
Yehor <yehor.chernyshov@gmail.com>
2023-03-26 16:27:50 +0200

    make app working

--- main.ts
+++ main.ts
@@ -1,6 +1,57 @@
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    countDownMinutes(15)
+    countDownMinutes(topButtonTime)
 })
+function setUp () {
+    led.setBrightness(80)
+    music.setVolume(255)
+    maxRow = 4
+    maxCol = 4
+    second = 1000
+    minute = 60 * second
+    leftButtonTime = 5
+    topButtonTime = 15
+    rightButtonTime = 25
+}
+function showIntro () {
+    basic.showString("pomodoro")
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.showNumber(leftButtonTime)
+    basic.pause(200)
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.showNumber(topButtonTime)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.showNumber(rightButtonTime)
+    basic.pause(200)
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        # . . . #
+        . . . . .
+        . . . . .
+        `)
+}
 function updatePointerPosition () {
     if (col == 0) {
         row += -1
@@ -10,26 +61,36 @@ function updatePointerPosition () {
     }
 }
 input.onButtonPressed(Button.A, function () {
-    countDownMinutes(5)
+    countDownMinutes(leftButtonTime)
 })
 function countDownMinutes (duration: number) {
     basic.showNumber(duration)
     basic.pause(100)
     reset()
-    step = Math.round(25 / duration)
-    wait = duration * minute
-    makeStep(step, wait, true)
-    complete()
+    wait = Math.round(duration * minute / 25)
+    makeSteps(wait)
+    complete(true)
 }
-function complete () {
-    basic.pause(5000)
+function complete (playSound: boolean) {
+    if (playSound) {
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 200, 600, 255, 0, 150, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
+    }
+    basic.showIcon(IconNames.Yes)
+    basic.pause(5 / second)
     basic.clearScreen()
 }
 input.onButtonPressed(Button.B, function () {
-    countDownMinutes(25)
+    countDownMinutes(rightButtonTime)
 })
 function updateLeftTimeImage () {
     led.toggle(col, row)
+}
+function makeSteps (wait: number) {
+    while (row >= 0) {
+        basic.pause(wait)
+        updateLeftTimeImage()
+        updatePointerPosition()
+    }
 }
 function reset () {
     basic.showLeds(`
@@ -42,31 +103,18 @@ function reset () {
     row = maxRow
     col = maxCol
 }
-function makeStep (step: number, wait: number, playSound: boolean) {
-    while (row >= 0) {
-        for (let index = 0; index < step; index++) {
-            updateLeftTimeImage()
-            updatePointerPosition()
-        }
-        basic.pause(wait)
-    }
-    basic.showIcon(IconNames.Yes)
-    if (playSound) {
-        music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 200, 600, 255, 0, 150, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
-    }
-}
 let wait = 0
-let step = 0
 let row = 0
 let col = 0
+let rightButtonTime = 0
+let leftButtonTime = 0
 let minute = 0
+let second = 0
 let maxCol = 0
 let maxRow = 0
-led.setBrightness(80)
-maxRow = 4
-maxCol = 4
-let second = 1000
-minute = 60 * second
+let topButtonTime = 0
+setUp()
+showIntro()
 basic.forever(function () {
 	
 })
